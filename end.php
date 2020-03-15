f606651cc3bd62fc2a5dde740b31e4073543dae8
2217wang <2677434110@qq.com>
2020-03-15 19:22:51 +0800

    2

--- end.php
+++ end.php
@@ -1,4 +1,7 @@
+//end。php
+
 <?php
+header("Content-Type: text/html;charset = utf-8");
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -10,7 +13,7 @@ mysqli_set_charset($conn, 'utf8');
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
 }
-$sql="SELECT *  FROM peroid WHERE test ='".'1'."'";
+$sql="SELECT * FROM peroid WHERE test ='".'1'."'";
 $result = mysqli_query($conn,$sql);
 if($row=mysqli_fetch_array($result)){
     $id=$row[0];
@@ -19,7 +22,7 @@ if($row=mysqli_fetch_array($result)){
     if ($conn->query($sql) === TRUE) {
         echo "已结束当前进行中的预约，进行抽签操作";
     }
-    $sql="SELECT *FROM appointment WHERE period_id ='".$id."'";
+    $sql="SELECT * FROM appointment WHERE period_id ='".$id."'";
     $result = mysqli_query($conn,$sql);
     while($row=mysqli_fetch_array($result)){
         if($row[3]<$count){
@@ -28,7 +31,8 @@ if($row=mysqli_fetch_array($result)){
             if ($conn->query($sql) === TRUE) {
                 echo $row[7]."获得".$row[3]."个口罩购买资格</br>";
             }
-        } else{
+        }
+        else{
             $sql="UPDATE appointment SET win ='".$count."'WHERE id ='".$row[0]."'";
             if ($conn->query($sql) === TRUE) {
                 echo $row[7]."获得".$count."个口罩购买资格</br>";
@@ -37,3 +41,4 @@ if($row=mysqli_fetch_array($result)){
         }
     }
 }
+$conn->close();
